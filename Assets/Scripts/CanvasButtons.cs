3269067269fd0cb586365bdc0141565bfc20bd5c
sarana <sarana.rb.ru@gmail.com>
2020-10-06 16:52:13 +0700

    Создание проигрыша

---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CanvasButtons : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadInstagram()
+    {
+        Application.OpenURL("https://instagram.com/burya_game?igshid=1sjym9bbmnefp");
+    }
+}
