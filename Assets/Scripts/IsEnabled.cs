abcb176220c38429d336e9a8ea40b5f871f679f3
sarana <sarana.rb.ru@gmail.com>
2020-10-19 03:20:37 +0700

    редактирование магазина

---
+++ Assets/Scripts/IsEnabled.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class IsEnabled : MonoBehaviour
+{
+    public int needToUnLock;
+    public Material blackMaterial;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("score") < needToUnLock)
+            GetComponent<MeshRenderer>().material = blackMaterial;
+    }
+}
